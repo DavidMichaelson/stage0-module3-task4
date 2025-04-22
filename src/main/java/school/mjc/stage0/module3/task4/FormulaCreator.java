5417e982146b0a05ca833ddd687a5e26c5499154
DavidMichaelson <dovydasbalciunas95@gmail.com>
2025-04-22 17:11:53 +0300

    done task

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -1,6 +1,10 @@
 package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
-    public static void main(String[] args) {
-    }
+    int a;
+    int b;
+
+    int formula1 = 9 * a * a - 5 * b + 2 + a - 7;
+    int formula2 = (a + b - 4 * a * b) / 2;
+    int formula3 = formula1 * formula2;
 }
